dc66caba0624913719666083598c6556b45c0b9f
Rodrigo Lins <rlinsdev@gmail.com>
2021-07-25 11:58:33 -0300

    computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,30 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <h2>Refs</h2>
-    <p>{{ninjaOne.name}} - {{ninjaOne.age}}</p>
-    <button @click="updateNinjaOne">Click me</button>
-    <h2>Reactive</h2>
-    <p>{{ninjaTwo.name}} - {{ninjaTwo.age}}</p>
-    <button @click="updateNinjaTwo">Click me Two</button>
+    <input type="text" v-model="search">
+    <p>Search term = {{search}}</p>
+    <div v-for="name in matchingNames" :key="name">{{name}}</div>
   </div>
 </template>
 
 <script>
 
-import { ref, reactive } from 'vue'
+import {computed, ref} from 'vue'
 
 export default {
-  name: 'Home',
-  setup () {    
+  name:'Home',
+  setup(){
+    const search = ref('')
+    const names = ref(['mario','luigi','yoshy' , 'bowser', 'koopa','tody'])
 
-    const ninjaOne = ref({name:'mario', age:30})
-    const ninjaTwo = reactive({name:'luigi',age:40})
+    const matchingNames = computed(()=>{
+      return names.value.filter((name)=>name.includes(search.value))
+    })
 
-    const updateNinjaOne =()=>{
-      ninjaOne.value.age = 40
-    }
-
-    const updateNinjaTwo =()=>{
-      ninjaTwo.age = 45
-    }
-
-    return {ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo}
-  },  
+      return {names, search, matchingNames}
+  }
 }
 </script>
+
+<style>
+</style>
